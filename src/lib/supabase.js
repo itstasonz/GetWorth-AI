813fa676b45fc0469b1bb9be2c1de47fdbadb14e
itstasonz <itstasonz@gmail.com>
2026-02-06 13:44:12 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // ⚠️ Replace with your own Supabase project URL and anon key
-const SUPABASE_URL = https://xbwxbdxuklrbnkpgonjc.supabase.co
+const SUPABASE_URL = 'https://xbwxbdxuklrbnkpgonjc.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhid3hiZHh1a2xyYm5rcGdvbmpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMzAwMzEsImV4cCI6MjA4NTcwNjAzMX0.eDVeOTgF7DX_BKgOoz7DnN5Gy5tikPG7r4DOA-FfnIY';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
